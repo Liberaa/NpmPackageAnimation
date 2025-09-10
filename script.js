f923b43aa6be3aa5b5dfa219f76b94eb059d911b
Lukas <lukas.soderlund.fanta@gmail.com>
2025-09-10 14:22:13 +0200

    div in the middle

--- script.js
+++ script.js
@@ -1,44 +1,45 @@
 // functions, export, 
 
-export function MoveDiv(move) {
+export function MoveDiv() {
     const element = document.createElement('div')
     element.id = 'player' // elemnts idlkl
     document.body.appendChild(element) // body basic html wallahi, putting the div inside the <body>
 
-    // css for the div element
     element.style.position = 'absolute'
     element.style.width = '60px'
     element.style.height = '60px'
     element.style.background = 'black'
-    element.style.left = '100px'
-    element.style.top = '100px'
 
-    let x = 100
-    let y = 100
+    // center horizontally by default
+    let x = window.innerWidth / 2 - 30 // 30 = half width of div
+    let y = window.innerHeight / 2 - 30 // optional vertical center
+    element.style.left = x + 'px'
+    element.style.top = y + 'px'
+
     let speed = 5
-    let keys = {}
-    // I Will set this vaule as true later In my if statments :D 
-    let up = false
-    let down = false
     let left = false
     let right = false
+    // I Will set this vaule as true later In my if statments :D 
 
-    // detect key press, maby use window.
     document.addEventListener('keydown', function(event) {
-        if(event.key == 'w') up = true // Something like this :O
+        if(event.key == 'a') left = true // Something like this :O
+        if(event.key == 'd') right = true
     })
 
     document.addEventListener('keyup', function(event) {
-        if(event.key == 'w') up = false // stop moving when 'w' is released
+        if(event.key == 'a') left = false
+        if(event.key == 'd') right = false
     })
 
-    // move loop
     function move() {
-        if(up) y = y - speed // move the div up
-        element.style.top = y + 'px' // update the di position
+        if(left) x -= speed
+        if(right) x += speed
 
-        requestAnimationFrame(move) // keep the loop going wallahi
+        element.style.left = x + 'px'
+        element.style.top = y + 'px'
+
+        requestAnimationFrame(move)
     }
 
-    move() 
+    move()
 }

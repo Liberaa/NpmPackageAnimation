cbb69875df0864115cc0b05f85f3ba0f263a58cb
Lukas <lukas.soderlund.fanta@gmail.com>
2025-09-15 16:50:19 +0200

    movement system ide'

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # NpmPackageAnimation
 I feel like it's pretty hard to animate player movement in js, so Im making a npm package :D 
+
+# It should help users to give a 2d game up and running fast. 
--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 export function MoveDiv(options = {}) {
     const player = document.createElement('div')
 
-const controlScheme = options.controlScheme || 'wsad' // Want to give user the choice to move the div like this :  x + jump  || or x and y no jump
+const controlScheme = options.controlScheme || 'wasd' // Want to give user the choice to move the div like this :  x + jump  || or x and y no jump
 
     player.style.width = '60px'
     player.style.height = '60px'
@@ -15,16 +15,19 @@ const controlScheme = options.controlScheme || 'wsad' // Want to give user the c
     let x = 200
     let y = 200
     let speed = 5
+    
     let leftPressed = false
     let rightPressed = false
     let upPressed = false
     let downPressed = false
-
     let jumPressed = false
+    let isJumping = false
+
+
     let velocity = 0
     let jumpPower = 5
     const gravity = 0.2
-     // const groundLevel = window.innerHeight
+     // const groundLevel = window.innerHeight - 60
 
     document.body.appendChild(player)
 
@@ -74,3 +77,31 @@ const controlScheme = options.controlScheme || 'wsad' // Want to give user the c
 
     return {} 
 }
+
+document.addEventListener('keydown', (event) => {
+    const key = event.key.toLowerCase()
+    if (controlScheme === 'wasd') {
+        if (key === 'a') leftPressed = true
+        if (key === 'd') rightPressed = true
+        if (key === 's') downPressed = true
+        if (key === 'w') upPressed = true
+    } else if ( controlScheme === 'platform'){
+        if (key === 'a') leftPressed = true
+        if (key === 'd') rightPressed = true
+        if(key === ' ' && !isJumping) {
+            jumPressed = true
+            isJumping = true
+            velocity = -jumpPower
+        }
+    }
+
+})
+
+document.addEventListener('keyup', event => {
+    const key = event.key.toLowerCase()
+    if(controlScheme === 'wasd') {
+
+    } else if (controlScheme === 'platform') {
+        
+    }
+})

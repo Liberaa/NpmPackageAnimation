0e145837624e7d1d1196fd817afa4b7e18ad86cb
Lukas <lukas.soderlund.fanta@gmail.com>
2025-09-15 21:29:54 +0200

    obstacle and // comments

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ import { MoveDiv } from './script.js'
 import { createObstacle } from './obstacle.js'
 
 MoveDiv({ controlScheme: 'platform'})
-createObstacle({ x: 400, y: 500, width: 120, height: 30, color: 'green' })
+createObstacle({ x: 400, y: 500, width: 1200, height: 300, color: 'green' })
+createObstacle({ x: 500, y: 580, width: 1000, height: 150, color: 'darkgreen' })
+
 
 // notes, if space in platform mode, goes down all the way
 // Do I want that? Maby don't know yet.
--- obstacle.js
+++ obstacle.js
@@ -5,7 +5,7 @@ export function createObstacle(options = {}) {
 
     const width = options.width || 100
     const height = options.height || 40
-    const color = options.color || 'red'
+    const color = options.color || 'darkgreen'
     const x = options.x || 300
     const y = options.y || window.innerHeight - height - 20
 
